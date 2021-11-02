a6a0387d6bb1d4157743cb8b3c2c0b0662667f15
gongluck <1039994845@qq.com>
2021-11-02 21:58:07 +0800

    518.零钱兑换-ii

---
+++ leetcode/518.零钱兑换-ii.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=518 lang=cpp
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    int change(int amount, vector<int> &coins)
+    {
+        std::vector<int> dp(amount + 1);
+        dp[0] = 1;
+        for (int &coin : coins)
+        {
+            for (int i = coin; i <= amount; i++)
+            {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+};
+// @lc code=end
