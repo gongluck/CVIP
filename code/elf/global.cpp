de3c175cdb070353e0a84e96ed0e7c1a38e3ac7f
gongluck <gongluck93@gmail.com>
2022-04-14 11:32:13 +0800

    elf application rewrite static variable itself via shell tools

--- code/elf/global.cpp
+++ code/elf/global.cpp
@@ -2,7 +2,7 @@
  * @Author: gongluck
  * @Date: 2022-04-14 10:49:56
  * @Last Modified by: gongluck
- * @Last Modified time: 2022-04-14 10:53:06
+ * @Last Modified time: 2022-04-14 11:30:47
  */
 
 // application rewrite it`s global variable via shell tools
@@ -10,6 +10,12 @@
 #include <stdlib.h>
 #define NAME2STR(name) (#name)
 int GGG = 1;
+int test()
+{
+  static int SSS = 100;
+  printf("%d\n", SSS);
+  return SSS;
+}
 int main(int argc, char *argv[])
 {
   if (argc == 3)
@@ -24,11 +30,21 @@ int main(int argc, char *argv[])
   {
     printf("%s\n", argv[0]);
     printf("%d\n", GGG);
+    test();
     srand(GGG);
     GGG = rand();
     char buf[1024] = {0};
+    // readelf -s a.out | grep GGG
+    // readelf -S a.out
+    // hexdump a.out -C -s 0x3014 -n 4
     sprintf(buf, "%s $(expr `printf %%d 0x$(readelf -s %s | grep %s | awk '{print $2}')` - `printf %%d 0x$(readelf -S %s | grep \" .data \" | awk '{print $4}')` + `printf %%d 0x$(readelf -S %s | grep \" .data \" | awk '{print $5}')`) %d",
             argv[0], argv[0], NAME2STR(GGG), argv[0], argv[0], GGG);
+    system(buf);
+    srand(GGG);
+    GGG = rand();
+    // rewrite static variable
+    sprintf(buf, "%s $(expr `printf %%d 0x$(readelf -s %s | grep %s | awk '{print $2}')` - `printf %%d 0x$(readelf -S %s | grep \" .data \" | awk '{print $4}')` + `printf %%d 0x$(readelf -S %s | grep \" .data \" | awk '{print $5}')`) %d",
+            argv[0], argv[0], NAME2STR(SSS), argv[0], argv[0], GGG);
     system(buf);
   }
   return 0;
