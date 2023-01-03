0d929ddb7bc7a7879427ec9bc3972db79b6a71c7
gongluck <gongluck93@gmail.com>
2023-01-03 23:23:59 +0800

    hello world

---
+++ code/elf/hello.c
@@ -0,0 +1,18 @@
+/*
+ * @Author: gongluck
+ * @Date: 2023-01-03 22:30:53
+ * @Last Modified by: gongluck
+ * @Last Modified time: 2023-01-03 22:38:59
+ */
+
+// gcc -E hello.c -o hello.i
+// gcc -S hello.c -o hello.s
+// gcc -c hello.c -o hello.o
+
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello World\n");
+  return 0;
+}
